abeca1f4e9f78093d0e1f674b16b880ede6679ac
Priyesh Soni <priyesh9792@gmail.com>
2023-09-13 15:42:41 +0530

    milestone3

--- me_jsdom_v2/index.js
+++ me_jsdom_v2/index.js
@@ -14,23 +14,31 @@
 
 // The append() or appendChild() methods on an element node can be used to add a new child element to it
 //-------------------------------------------------------------------------------------------------
-const pElement=document.createElement("h3")
-pElement.innerText="innerText property refers to the text inside an element";
-const divElement=document.getElementById("facts-list");
-console.log(divElement);
-divElement.appendChild(pElement);
+// const pElement=document.createElement("h3")
+// pElement.innerText="innerText property refers to the text inside an element";
+// const divElement=document.getElementById("facts-list");
+// console.log(divElement);
+// divElement.appendChild(pElement);
 
 //-------------------------------------------------------------------------------------------------
 
-function addNewFact(factText){
-    const pElement=document.createElement("h3");
-    console.log(pElement.innerText)
-    pElement.innerText=factText;
-    console.log(pElement.innerText)
-    const divElement=document.getElementById("facts-list");
-    console.log(divElement);
-    divElement.appendChild(pElement);
-    console.log(divElement)
-}
-addNewFact("innerText property refers to the text inside an element")
-addNewFact("append() or appendChild() methods can add a new child element")
+// function addNewFact(factText){
+//     const pElement=document.createElement("h3");
+//     console.log(pElement.innerText)
+//     pElement.innerText=factText;
+//     console.log(pElement.innerText)
+//     const divElement=document.getElementById("facts-list");
+//     console.log(divElement);
+//     divElement.appendChild(pElement);
+//     console.log(divElement)
+// }
+// addNewFact("innerText property refers to the text inside an element")
+// addNewFact("append() or appendChild() methods can add a new child element")
+
+//-------------------------------------------------------------------------------------------------
+
+const divElement=document.getElementById("facts-list");
+console.log(divElement.innerHTML)
+   divElement.innerHTML=
+"<p>Browser creates DOM from HTML content</p><p>Developers can access the DOM using the DOM APIs</p><p>innerHTML property refers to the HTML inside an element</p>"
+   
