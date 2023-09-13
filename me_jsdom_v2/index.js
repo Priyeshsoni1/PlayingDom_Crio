367c8fae22f904e57442d0ff06c22a74bd1d9dba
Priyesh Soni <priyesh9792@gmail.com>
2023-09-13 15:12:04 +0530

    milestone1

--- me_jsdom_v2/index.js
+++ me_jsdom_v2/index.js
@@ -1,1 +1,6 @@
 console.log("Hello, from JS!");
+let innerText=document.querySelector("h1").textContent
+console.log(innerText)
+let practice=document.getElementsByTagName("h1")[0].innerText
+console.log(practice);
+document.getElementsByTagName("h1")[0].innerText="Practicing DOM Manipulation"
