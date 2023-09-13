1b5981e7ba383f66ed2d023a5f33e9d2d12a59b7
Priyesh Soni <priyesh9792@gmail.com>
2023-09-13 15:33:21 +0530

    milestone 2

--- me_jsdom_v2/index.js
+++ me_jsdom_v2/index.js
@@ -1,6 +1,36 @@
-console.log("Hello, from JS!");
-let innerText=document.querySelector("h1").textContent
-console.log(innerText)
-let practice=document.getElementsByTagName("h1")[0].innerText
-console.log(practice);
-document.getElementsByTagName("h1")[0].innerText="Practicing DOM Manipulation"
+// console.log("Hello, from JS!");
+// let innerText=document.querySelector("h1").textContent
+// console.log(innerText)
+// let practice=document.getElementsByTagName("h1")[0].innerText
+// console.log(practice);
+// document.getElementsByTagName("h1")[0].innerText="Practicing DOM Manipulation"
+
+//--------------------------------------------------------------------------------------------------
+// Creating new objects
+// You updated the text of an HTML element in the previous milestone. Let’s look at how to create new ones now.
+
+
+// The document.createElement() method can be used for creating a new element node
+
+// The append() or appendChild() methods on an element node can be used to add a new child element to it
+//-------------------------------------------------------------------------------------------------
+const pElement=document.createElement("h3")
+pElement.innerText="innerText property refers to the text inside an element";
+const divElement=document.getElementById("facts-list");
+console.log(divElement);
+divElement.appendChild(pElement);
+
+//-------------------------------------------------------------------------------------------------
+
+function addNewFact(factText){
+    const pElement=document.createElement("h3");
+    console.log(pElement.innerText)
+    pElement.innerText=factText;
+    console.log(pElement.innerText)
+    const divElement=document.getElementById("facts-list");
+    console.log(divElement);
+    divElement.appendChild(pElement);
+    console.log(divElement)
+}
+addNewFact("innerText property refers to the text inside an element")
+addNewFact("append() or appendChild() methods can add a new child element")
